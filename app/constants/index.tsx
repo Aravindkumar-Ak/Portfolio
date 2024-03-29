a694a5657dc79b64c68cce97b70c17d2a52b5de9
Aravindkumar <Aravindkumarak1147@gmail.com>
2024-03-29 13:17:10 +0530

    portfolio updated

---
+++ app/constants/index.tsx
@@ -0,0 +1,123 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+export const NavLinks = [
+    {
+      name: "/",
+      icon: RxHome,
+      link: "/",
+    },
+    {
+      name: "/my-skills",
+      icon: RxPerson,
+      link: "/my-skills",
+    },
+    {
+      name: "/my-projects",
+      icon: RxDashboard,
+      link: "/my-projects",
+    },
+    {
+      name: "/contact-me",
+      icon: RxClipboard,
+      link: "/contact-me",
+    },
+  ];
+
+  export const SkillData = [
+    {
+      name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      name: "JavaScript",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      name: "Redux",
+      Image: "/redux.png",
+      width: 80,
+      height: 80,
+    },
+  
+    {
+      name: "TypeScript",
+      Image: "/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+    
+   
+    {
+      name: "Node js",
+      Image: "/node-js.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      name: "Mongo db",
+      Image: "/mongodb.png",
+      width: 40,
+      height: 40,
+    },
+  ];
+  
+  
+  
+  export const Projects = [
+    {
+      title: "Netflix-GPT",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+     
+      width: 400,
+      height: 400,
+    },
+    {
+      title: "Ecommerce Website",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+     
+      width: 400,
+      height: 400,
+    },
+    {
+      title: "Food Delivery App",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+      
+      width: 300,
+      height: 400,
+    },
+    {
+      title: "Chat Application",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+     
+      width: 100,
+      height: 400,
+    },
+  ];
+  
---
+++ app/my-projects/page.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import { Projects } from '../constants';
+
+
+const Page = () => {
+  return (
+    <div
+    style={{ backgroundImage: "url(/bg.jpg)" }}
+    className="h-screen w-screen flex items-center justify-center bg-cover bg-center"
+  >
+    <div className="flex flex-col gap-20 max-w-[80%] text-center items-center">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500 text-[50px]">
+         Projects
+        </h1>
+        
+      </div >
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+        }}
+        speed={5000}
+        modules={[Autoplay]}
+        className="max-w-[80%]"
+      >
+        {Projects.map((project, index) => (
+          <SwiperSlide key={index}>
+             
+            {/* <Image 
+            src={project.src}
+            alt={project.title}
+            width={400}
+            height={400}
+           
+           
+            /> */}
+            <div className='relative flex justify-center h-[400px] w-[75%] bg-gray-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-none bg-opacity-20 border border-gray-100'>
+          
+               <p className='font-semibold text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-500 text-[50px] absolute top-[32%]'>{project.title}</p>
+               </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+ 
+    </div>
+  </div>
+  )
+}
+
+export default Page
